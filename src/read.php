b54221329e6f55fa2bc17de5e4212df61ef28c76
rock <pedrofiliperocha2001@gmail.com>
2022-06-05 22:41:35 +0100

    modal finally fucking fixed

--- src/read.php
+++ src/read.php
@@ -60,24 +60,22 @@ if(isset ($_POST['pesquisa'])) {
 				<td><?PHP echo $row ["nome"]?></td>
 				<td><?PHP echo $row ["email"]?></td>
 				<td><a href="update.php?codigo=<?PHP echo $row ["codigo"]?>">Alterar</a></td>
-				<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal<?PHP echo $row ["codigo"]?>">Eliminar</button></a></td>
+				<td><button type="button" class="btn btn-primary" data-bs-toggle="modal"data-bs-target="#exampleModal<?PHP echo $row ["codigo"]?>">delete</button></td>
               
         <!-- Modal -->
-<div class="modal fade" id="exampleModal<?PHP echo $row ["codigo"]?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div id="exampleModal<?PHP echo $row ["codigo"]?>" class="modal fade" role="dialog" tabindex="-1">
+  <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+        Tem a certeza que pretrende apagar o registo com o código <?PHP echo $row ["codigo"]?> ? 
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+        <a type="button" class="btn btn-danger" href="delete.php?codigo=<?PHP echo $row ["codigo"]?>">Yes</a>
       </div>
     </div>
   </div>
